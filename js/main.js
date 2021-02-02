9810a1cad1a43b80b3af9ee00057b15260419ae5
Maja Jensen <eaamnje@students.eaaa.dk>
2021-02-02 10:43:14 +0100

    Default

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// import SpaService from "./services/spa.js";
+// import LoaderService from "./services/loader.js";
+
+
+// // Declare and init services
+// let spaService = new SpaService("home");
+// let loaderService = new LoaderService();
+// loaderService.show(false);
+
+// window.pageChange = function () {
+//     spaService.pageChange();
+// }
